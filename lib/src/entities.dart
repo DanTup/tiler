75846f37038c2fdcf02fd2a6c6cee20d3b06dd47
Danny Tuppeny <danny@tuppeny.com>
2019-05-05 17:33:10 +0100

    Add code for decoding/decompressing
    Needs work, curerntly assumes ZLib!

--- lib/src/entities.dart
+++ lib/src/entities.dart
@@ -7,3 +7,23 @@ import 'package:json_annotation/json_annotation.dart';
 part 'entities.g.dart';
 part 'entities_generated.dart';
 
+final _zlib = ZLibCodec();
+
+List<int> decodeData(dynamic value) {
+  if (value is String) {
+    // TODO: We shouldn't assume zlib?
+    final compressedBytes = base64Decode(value);
+    final bytes = Uint8List.fromList(_zlib.decode(compressedBytes));
+    final byteData = ByteData.view(bytes.buffer);
+    return List.generate(
+      (byteData.lengthInBytes / 4).floor(),
+      (i) => byteData.getUint32(i * 4, Endian.little),
+    );
+  } else if (value is List) {
+    return value.cast<int>();
+  } else if (value == null) {
+    return null;
+  } else {
+    throw Exception('Unknown data type: ${value.runtimeType}');
+  }
+}
