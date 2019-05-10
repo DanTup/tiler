7cfeb15159c09a8dc88855de580bb2cc2f91de3e
Danny Tuppeny <danny@tuppeny.com>
2019-05-10 14:48:17 +0100

    Goldens are macOS-specific

--- test/encoding_test.dart
+++ test/encoding_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'utils.dart';
@@ -5,12 +7,12 @@ import 'utils.dart';
 void main() {
   testWidgets('csv', (tester) async {
     await expectMapRender(tester, 'encoding/csv', 'encoding/output');
-  });
+  }, skip: !Platform.isMacOS);
   // testWidgets('base64_uncompressed', (tester) async {
   //   await expectMapRender(
   //       tester, 'encoding/base64_uncompressed', 'encoding/output');
   // });
   testWidgets('base64_zlib', (tester) async {
     await expectMapRender(tester, 'encoding/base64_zlib', 'encoding/output');
-  });
+  }, skip: !Platform.isMacOS);
 }
