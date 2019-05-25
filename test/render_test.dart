d5ba2e7c9943400a569e79eb0a92ba095312d90f
Danny Tuppeny <danny@tuppeny.com>
2019-05-25 13:20:44 +0100

    Add isometric test

--- test/render_test.dart
+++ test/render_test.dart
@@ -2,11 +2,12 @@ import 'utils.dart';
 
 void main() {
   testMapRender('embedded/tileset');
-  testMapRender('encoding/base64_uncompressed', 'encoding/output');
-  testMapRender('encoding/base64_zlib', 'encoding/output');
-  testMapRender('encoding/csv', 'encoding/output');
+  testMapRender('encoding/base64_uncompressed', goldenName: 'encoding/output');
+  testMapRender('encoding/base64_zlib', goldenName: 'encoding/output');
+  testMapRender('encoding/csv', goldenName: 'encoding/output');
   testMapRender('offsets/layers');
   testMapRender('translations/mirrored');
   testMapRender('translations/rotated_and_mirrored');
   testMapRender('translations/rotated');
+  testMapRender('orientation/isometric', scale: 0.25);
 }
